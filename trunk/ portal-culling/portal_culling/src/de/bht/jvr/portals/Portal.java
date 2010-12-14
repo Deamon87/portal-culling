2094d3dd65a1cefeb981178d83dc7b21f5aae200
DerRM123 <DerRM123@480b27eb-8ba1-0354-dace-e47c453a7b79>
2010-12-14 15:11:58 +0000

    some basics

--- trunk/ portal-culling/portal_culling/src/de/bht/jvr/portals/Portal.java
+++ trunk/ portal-culling/portal_culling/src/de/bht/jvr/portals/Portal.java
@@ -1,5 +1,55 @@
 package de.bht.jvr.portals;
 
+import de.bht.jvr.core.CameraNode;
+import de.bht.jvr.math.Vector3;
+
 public class Portal {
 
+	private float height;
+	private float width;
+	private Vector3 direction;
+	private CameraNode camera;
+	
+	public Portal() {
+		
+	}
+	
+	public Portal(float height, float width, Vector3 direction, CameraNode cam) {
+		this.height = height;
+		this.width = width;
+		this.direction = direction;
+		this.camera = cam;
+	}
+	
+	public void setHeight(float height) {
+		this.height = height;
+	}
+	
+	public float getHeight() {
+		return this.height;
+	}
+	
+	public void setWidth(float width) {
+		this.width = width;
+	}
+	
+	public float getWidth() {
+		return this.width;
+	}
+	
+	public void setDirection(Vector3 direction) {
+		this.direction = direction;
+	}
+	
+	public Vector3 getDirection() {
+		return this.direction;
+	}
+	
+	public void setCamera(CameraNode cam) {
+		this.camera = cam;
+	}
+	
+	public CameraNode getCamera() {
+		return this.camera;
+	}
 }
--- trunk/ portal-culling/portal_culling/src/de/bht/jvr/portals/PortalConnection.java
+++ trunk/ portal-culling/portal_culling/src/de/bht/jvr/portals/PortalConnection.java
@@ -2,4 +2,28 @@ package de.bht.jvr.portals;
 
 public class PortalConnection {
 
+	private Portal portal1;
+	private Portal portal2;
+	
+	public PortalConnection(Portal portal1, Portal portal2) {
+		this.portal1 = portal1;
+		this.portal2 = portal2;
+	}
+	
+	public void setPortal1(Portal portal1) {
+		this.portal1 = portal1;
+	}
+	
+	public Portal getPortal1() {
+		return this.portal1;
+	}
+	
+	public void setPortal2(Portal portal2) {
+		this.portal2 = portal2;
+	}
+	
+	public Portal getPortal2() {
+		return this.portal2;
+	}
+	
 }
