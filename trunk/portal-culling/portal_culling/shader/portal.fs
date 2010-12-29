105b2d2def5313455fc9d4027f77855ec3495fe1
DerRM123 <DerRM123@480b27eb-8ba1-0354-dace-e47c453a7b79>
2010-12-29 14:23:26 +0000

    some Portal Rendering

---
+++ trunk/portal-culling/portal_culling/shader/portal.fs
@@ -0,0 +1,28 @@
+uniform mat4 jvr_ProjectionMatrix;
+uniform sampler2D jvr_PortalTexture;
+
+uniform float waveTime;
+uniform float waveScale;
+
+varying vec3 eyeDir;
+varying vec4 vertex;
+varying vec2 texCoord;
+
+void main (void)
+{	
+	// Moving from [-1,1] to [0,1]
+	mat4 DCtoTC = mat4(	0.5, 0.0, 0.0, 0.0,
+						0.0, 0.5, 0.0, 0.0,
+						0.0, 0.0, 0.5, 0.0,
+						0.5, 0.5, 0.5, 1.0);
+
+  
+    // Project an homogenize vertex to yield texture coordinates.
+	vec4 vertexNDC = DCtoTC * jvr_ProjectionMatrix * vertex;	
+	vec2 texCoord = vertexNDC.xy / vertexNDC.w;
+
+	texCoord.x = 1.0-texCoord.x;
+	
+	// Make a cyanish tint.
+	gl_FragColor = texture2D(jvr_PortalTexture, texCoord);
+}
