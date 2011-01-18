0d87f451748228a0b04d4a1f7624185790efec09
DerRM123 <DerRM123@480b27eb-8ba1-0354-dace-e47c453a7b79>
2011-01-18 14:01:07 +0000

    encapsulation somewhat completed

--- trunk/portal-culling/portal_culling/src/de/bht/jvr/portals/Door.java
+++ trunk/portal-culling/portal_culling/src/de/bht/jvr/portals/Door.java
@@ -1,17 +1,53 @@
 package de.bht.jvr.portals;
 
+import java.awt.Color;
+import java.io.File;
+
+import de.bht.jvr.core.Finder;
+import de.bht.jvr.core.ShaderMaterial;
+import de.bht.jvr.core.ShaderProgram;
+import de.bht.jvr.core.ShapeNode;
 import de.bht.jvr.core.pipeline.Pipeline;
+import de.bht.jvr.core.uniforms.UniformBool;
 
 public class Door extends Portal {
 
+	private static ShaderProgram shaderProg;
+	
+	static {
+		try {
+			shaderProg = new ShaderProgram(new File("shader/portal.fs"), new File("shader/portal.vs"));			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public Door(Pipeline p, String name) throws Exception {
 		super(p, name);
+		ShaderMaterial shaderMat = new ShaderMaterial("AMBIENT", shaderProg);
+		shaderMat.setMaterialClass(this.getName() + "Mat");
+		
+		ShapeNode portalShape = Finder.find(this, ShapeNode.class, null);
+		portalShape.setMaterial(shaderMat);
+		
+		PortalList.add(this);
+		
+		this.render();
 	}
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
+		getPipeline().setUniform("jvr_UseClipPlane0", new UniformBool(false));
+		getPipeline().setUniform("jvr_UseClipPlane1", new UniformBool(false));
 		
+		getPipeline().createFrameBufferObject(this.getName() + "FBO", false, 1, 1, 0);
+		getPipeline().switchFrameBufferObject(this.getName() + "FBO");
+		getPipeline().switchCamera(this.getCamera());
+		getPipeline().clearBuffers(true, true, new Color(121, 188, 255));
+		//getPipeline().setUniform("jvr_UseClipPlane0", new UniformBool(false));
+		//getPipeline().setUniform("jvr_UseClipPlane1", new UniformBool(true));
+		getPipeline().drawGeometry("AMBIENT", null);
+		getPipeline().doLightLoop(true, true).drawGeometry("LIGHTING", null);
 	}
 
 	@Override
